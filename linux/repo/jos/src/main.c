c9fe65320f6782ed451b9d2c690945ff556f93d3
linux <yihsihuang@gmail.com>
2014-10-22 09:34:13 -0700

    initial commit

---
+++ linux/repo/jos/src/main.c
@@ -0,0 +1,15 @@
+// main.c -- Defines the C-code kernel entry point, calls initialisation routines.
+//           Made for JamesM's tutorials <www.jamesmolloy.co.uk>
+
+#include "monitor.h"
+
+int main(struct multiboot *mboot_ptr)
+{
+    // Initialise the screen (by clearing it)
+    monitor_clear();
+    // Write out a sample string
+    monitor_write("Hello, world!");
+ //   asm volatile ("int $0x3");
+ //   asm volatile ("int $0x4");
+    return 0;
+}
